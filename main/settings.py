04e862a5e4684f0c9a367f0c4a616be6ab957cd1
mildred-prog <mildylicious@gmail.com>
2025-04-03 23:20:59 +0100

    readies code for deploy

--- main/settings.py
+++ main/settings.py
@@ -26,9 +26,9 @@ if os.path.exists('env.py'):
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.01', '.herokuapp.com']
 
 
 # Application definition
