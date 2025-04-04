a5b56fab8088bb88017b86117af8697bbe2ba72c
mildred-prog <mildylicious@gmail.com>
2025-04-04 17:14:03 +0100

    Deployment to hereoku

--- env.py
+++ env.py
@@ -3,3 +3,5 @@ import os
 os.environ.setdefault(
     "SECRET_KEY", "django-insecure-fg0*l5sy83ok3gi&u33ds_fi_o=8j_d)n&zfbvwi@c!q(u(7m6"
 )
+os.environ['DEVELOPMENT'] = 'True'
+
--- main/settings.py
+++ main/settings.py
@@ -26,7 +26,7 @@ if os.path.exists('env.py'):
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 
