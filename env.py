8a72b512d3ad7e1b090d2a5ef9931695dfb36f89
mildred-prog <mildylicious@gmail.com>
2025-04-10 17:10:00 +0100

    Remove env file from version control

--- env.py
+++
@@ -1,7 +0,0 @@
-import os
-
-os.environ.setdefault(
-    "SECRET_KEY", "django-insecure-fg0*l5sy83ok3gi&u33ds_fi_o=8j_d)n&zfbvwi@c!q(u(7m6"
-)
-os.environ['DEVELOPMENT'] = 'True'
-
--- recipes/views.py
+++ recipes/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.views.generic import CreateView, ListView
 
-# Create your views here.
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+from .models import Recipe
+from .forms import RecipeForm
+
+
+class Recipes(ListView):
+    """View all recipes"""
+
+    template_name = "recipes/recipes.html"
+    model = Recipe
+    context_object_name = "recipes"
+
+
+class AddRecipe(LoginRequiredMixin, CreateView):
+    """Add recipe view"""
+
+    template_name = "recipes/add_recipe.html"
+    model = Recipe
+    form_class = RecipeForm
+    success_url = "/recipes/"
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super(AddRecipe, self).form_valid(form)
