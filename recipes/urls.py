61e1bf95c9c5298f1ff61501ecb86ab2ae6d3ca8
mildred-prog <mildylicious@gmail.com>
2025-04-10 17:47:36 +0100

    Resolve merge conflict in recipes views and url

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,19 +1,12 @@
 from django.urls import path
-<<<<<<< HEAD
+
 from .views import (
     AddRecipe, Recipes,
     # RecipeDetail, DeleteRecipe,
     # EditRecipe
 )
 
-
 urlpatterns = [
     path("add/", AddRecipe.as_view(), name="add_recipe"),
     path("", Recipes.as_view(), name="recipes"),
-=======
-from .views import AddRecipe
-
-urlpatterns = [
-    path('', AddRecipe.as_view(), name='add_recipe'),
->>>>>>> c4143b184631830becd95ccf91d2202f41e79ccf
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -1,15 +1,8 @@
-<<<<<<< HEAD
 from django.views.generic import CreateView, ListView
-=======
-from django.views.generic import CreateView
->>>>>>> c4143b184631830becd95ccf91d2202f41e79ccf
-
 from django.contrib.auth.mixins import LoginRequiredMixin
 
 from .models import Recipe
 from .forms import RecipeForm
-
-<<<<<<< HEAD
 
 class Recipes(ListView):
     """View all recipes"""
@@ -30,15 +23,4 @@ class AddRecipe(LoginRequiredMixin, CreateView):
     def form_valid(self, form):
         form.instance.user = self.request.user
         return super(AddRecipe, self).form_valid(form)
-=======
-class AddRecipe(LoginRequiredMixin, CreateView):
-    """ Add recipe view """
-    template_name = 'recipes/add_recipe.html'
-    model = Recipe
-    form_class = RecipeForm
-    success_url = '/recipes/'
-
-    def form_valid(self, form):
-        form.instance.user = self.request.user
-        return super(AddRecipe).form_valid(form)
->>>>>>> c4143b184631830becd95ccf91d2202f41e79ccf
+    
