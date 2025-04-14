313b52010cc4bfea9d5ca67896a21674b0fd4f4d
mildred-prog <mildylicious@gmail.com>
2025-04-14 16:02:05 +0100

    Add EditProfile view for user profile editing functionality

--- profiles/views.py
+++ profiles/views.py
@@ -1,8 +1,8 @@
-from django.views.generic import TemplateView
-
+from django.views.generic import TemplateView, UpdateView
+from django.contrib.auth.mixins import UserPassesTestMixin, LoginRequiredMixin
 
 from .models import Profile
-
+from .forms import ProfileForm
 
 
 class Profiles(TemplateView):
@@ -14,9 +14,21 @@ class Profiles(TemplateView):
         profile = Profile.objects.get(user=self.kwargs["pk"])
         context = {
             "profile": profile,
+            'form': ProfileForm(instance=profile)
         }
 
         return context
 
 
+class EditProfile(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    """Edit a profile"""
 
+    form_class = ProfileForm
+    model = Profile
+
+    def form_valid(self, form):
+        self.success_url = f'/profiles/user/{self.kwargs["pk"]}/'
+        return super().form_valid(form)
+
+    def test_func(self):
+        return self.request.user == self.get_object().user
