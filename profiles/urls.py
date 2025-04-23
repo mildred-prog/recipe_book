896b72914ea8a6406285e1a7f8b3cc008b54af46
mildred-prog <mildylicious@gmail.com>
2025-04-23 15:41:09 +0100

    Fix URL patterns to use integer type for primary key in profiles

--- profiles/urls.py
+++ profiles/urls.py
@@ -3,7 +3,7 @@ from .views import Profiles, EditProfile
 
 
 urlpatterns = [
-    path("user/<slug:pk>/", Profiles.as_view(), name="profile"),
-    path("edit/<slug:pk>/", EditProfile.as_view(), name="edit_profile"),
+    path("user/<int:pk>/", Profiles.as_view(), name="profile"),
+    path("edit/<int:pk>/", EditProfile.as_view(), name="edit_profile"),
     
 ]
