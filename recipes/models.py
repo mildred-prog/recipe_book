a89a86122d072fd1369ef153ac960bbb25739913
mildred-prog <mildylicious@gmail.com>
2025-04-30 20:45:26 +0100

    Fixed  line too long errors for PEP8 testing

--- recipes/models.py
+++ recipes/models.py
@@ -42,9 +42,22 @@ class Recipe(models.Model):
         blank=False,
         null=False,
     )
-    image_alt = models.CharField(max_length=100, null=False, blank=False)
-    meal_type = models.CharField(max_length=50, choices=MEAL_TYPES, default="Breakfast")
-    cuisine_type = models.CharField(max_length=50, choices=CUISINE_TYPES, default="african")
+    image_alt = models.CharField(
+        max_length=100,
+        null=False,
+        blank=False
+    )
+    meal_type = models.CharField(
+        max_length=50,
+        choices=MEAL_TYPES,
+        default="Breakfast"
+    )
+    cuisine_type = models.CharField(
+        max_length=50,
+        choices=CUISINE_TYPES,
+        default="african"
+    )
+
     calories = models.IntegerField()
     posted_date = models.DateTimeField(auto_now_add=True)
 
