6d394bbe6d8b134006ebb53e4eb67e68ef5aa01d
mildred-prog <mildylicious@gmail.com>
2025-05-03 14:43:22 +0100

    Update settings

--- main/settings.py
+++ main/settings.py
@@ -24,7 +24,7 @@ if os.path.exists('env.py'):
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = False
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
