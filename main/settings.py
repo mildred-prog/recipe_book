8037677cf3a731712c64077e69263e94a58bc91d
mildred-prog <mildylicious@gmail.com>
2025-05-03 14:56:44 +0100

    Resolve issues in settings

--- main/settings.py
+++ main/settings.py
@@ -24,10 +24,10 @@ if os.path.exists('env.py'):
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = False
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.herokuapp.com']
 
